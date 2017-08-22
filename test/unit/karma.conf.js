8f2574ed28942095027301565cbd28b21daeff72
Matthias Adler <macedigital@users.noreply.github.com>
2017-08-22 13:38:56 +0200

    chore: Use chrome-headless for unit-tests
    Drop phantomjs and use ChromeHeadless instead, which behaves pretty much like a real browser.
    
    References:
    http://cvuorinen.net/2017/05/running-angular-tests-in-headless-chrome/
    https://developers.google.com/web/updates/2017/06/headless-karma-mocha-chai

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,5 +1,5 @@
 // This is a karma config file. For more details see
-//   http://karma-runner.github.io/0.13/config/configuration-file.html
+//   https://karma-runner.github.io/1.0/config/configuration-file.html
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
@@ -7,17 +7,19 @@ const webpackConfig = require('../../build/webpack.test.conf');
 
 module.exports = function karmaConfig(config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-    reporters: ['spec', 'coverage'],
+    frameworks: ['mocha', 'sinon-chai'],
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap'],
     },
+    browsers: ['ChromeHeadless'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    singleRun: true,
+    concurrency: Infinity,
+    reporters: ['spec', 'coverage'],
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true,
@@ -28,6 +30,18 @@ module.exports = function karmaConfig(config) {
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' },
       ],
+    },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+          '--remote-debugging-port=9222', // Without a remote debugging port, Google Chrome exits immediately.
+        ],
+      },
     },
   });
 };

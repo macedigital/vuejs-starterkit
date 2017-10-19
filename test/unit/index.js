c9d75ee5215014c35d977b23f370a1b8e035bf27
Matthias Adler <macedigital@users.noreply.github.com>
2017-10-19 13:41:50 +0200

    chore(test): hide "use dev-tools" message in tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
+Vue.config.devtools = false;
 
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/);

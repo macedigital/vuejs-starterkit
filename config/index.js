c1d70f0bb283e6f51b87fed8f6e4dc316d3aeeea
Matthias Adler <macedigital@users.noreply.github.com>
2017-11-14 11:44:06 +0100

    chore(build): add html-files to gzip-extensions

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzipExtensions: ['js', 'css', 'html'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`

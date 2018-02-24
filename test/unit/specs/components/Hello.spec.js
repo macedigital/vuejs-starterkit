22c70507de8d3c105e55d1d6b9d1d392bff18be8
Matthias Adler <macedigital@users.noreply.github.com>
2018-02-24 19:11:07 +0100

    chore: use avoriaz for testing components

---
+++ test/unit/specs/components/Hello.spec.js
@@ -0,0 +1,35 @@
+import { mount } from 'avoriaz';
+import Hello from '@/components/Hello';
+
+describe('Hello.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Hello);
+  });
+
+  it('should render headline based on `msg` data-property', () => {
+    const { msg } = wrapper.data();
+    expect(wrapper.find('.hello h1')[0].text()).to.equal(msg);
+  });
+
+  it('should have reactive `msg` data-property', () => {
+    const msg = 'Something else coming in';
+    wrapper.setData({
+      msg,
+    });
+    expect(wrapper.find('.hello h1')[0].text()).to.equal(msg);
+  });
+
+  it('should render 9 links', () => {
+    expect(wrapper.find('.hello a').length).to.equal(9);
+  });
+
+  it('should render correct sub-headline with essential links', () => {
+    expect(wrapper.find('.hello h2')[0].text()).to.equal('Essential Links');
+  });
+
+  it('should render correct sub-headline with ecosystem links', () => {
+    expect(wrapper.find('.hello h2')[1].text()).to.equal('Ecosystem');
+  });
+});
